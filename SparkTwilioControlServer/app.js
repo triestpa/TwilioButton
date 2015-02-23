207e383135e33d67c707d5bdc8e73a44930cd93f
triestpa <triestpa@grinnell.edu>
2015-02-22 22:56:48 -0600

    Integrate with custom spark firmware

--- SparkTwilioControlServer/app.js
+++ SparkTwilioControlServer/app.js
@@ -4,11 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var spark = require('spark');
+var twilio = require('twilio');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+var client = new twilio.RestClient('ACfb7b4de6de01b6b7af8e317fcbefe060', 'afc8a5425ab2844dce905e8ddc68f060');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -56,5 +60,30 @@ app.use(function(err, req, res, next) {
     });
 });
 
+spark.on('login', function(err, body) {
+  console.log('API call completed on Login event:', body);
+});
+
+spark.login({accessToken: 'd1f902debd706ffcfe30a6b59e4c6247b0012d6d'});
+
+//Subscribe to global test event
+spark.onEvent('make-call', function(data) {
+  console.log("Making Call Now");
+  client.makeCall({
+        to:'6089218792',
+        from:'+1 781-917-3133',
+        url:'http://104.131.31.123:3000/twiml',
+        record: "false",
+        Method: "POST"
+    }, function(err, call) {
+        if (!err) {
+            console.log('This call\'s unique ID is: ' + call.sid);
+            console.log('This call was created at: ' + call.dateCreated);
+        }
+        else {
+            console.log(error);
+        }
+    });
+});
 
 module.exports = app;
